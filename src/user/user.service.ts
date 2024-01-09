a252a19c6a0eeea336490d88b08c68ea0781127b
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2024-01-09 12:12:18 +0530

    add address api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async createAddress(body): Promise<any> {
+    const response = await this.prismaService.address.create({
+      data: body,
+    });
+    return response;
+  }
+}
